24fadd268be4e1591711c66010de582fbfcc09fe
Saeed MJ <treegp@gmail.com>
2022-07-10 11:47:13 +0430

    Select Databases

---
+++ ConnectionHandler/MainForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace ConnectionHandler
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DataSourceTextBox = new System.Windows.Forms.TextBox();
+            this.UserIdCheckBox = new System.Windows.Forms.CheckBox();
+            this.UserIdTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DatabasesComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(627, 19);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(85, 25);
+            this.ConnectButton.TabIndex = 4;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Data Source";
+            // 
+            // DataSourceTextBox
+            // 
+            this.DataSourceTextBox.Location = new System.Drawing.Point(113, 20);
+            this.DataSourceTextBox.Name = "DataSourceTextBox";
+            this.DataSourceTextBox.Size = new System.Drawing.Size(100, 22);
+            this.DataSourceTextBox.TabIndex = 0;
+            // 
+            // UserIdCheckBox
+            // 
+            this.UserIdCheckBox.AutoSize = true;
+            this.UserIdCheckBox.Location = new System.Drawing.Point(249, 21);
+            this.UserIdCheckBox.Name = "UserIdCheckBox";
+            this.UserIdCheckBox.Size = new System.Drawing.Size(74, 20);
+            this.UserIdCheckBox.TabIndex = 1;
+            this.UserIdCheckBox.Text = "User ID";
+            this.UserIdCheckBox.UseVisualStyleBackColor = true;
+            this.UserIdCheckBox.CheckedChanged += new System.EventHandler(this.UserIdCheckBox_CheckedChanged);
+            // 
+            // UserIdTextBox
+            // 
+            this.UserIdTextBox.Enabled = false;
+            this.UserIdTextBox.Location = new System.Drawing.Point(329, 20);
+            this.UserIdTextBox.Name = "UserIdTextBox";
+            this.UserIdTextBox.Size = new System.Drawing.Size(100, 22);
+            this.UserIdTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(435, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Enabled = false;
+            this.PasswordTextBox.Location = new System.Drawing.Point(508, 20);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.Size = new System.Drawing.Size(100, 22);
+            this.PasswordTextBox.TabIndex = 3;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(28, 89);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(684, 378);
+            this.checkedListBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Databases";
+            // 
+            // DatabasesComboBox
+            // 
+            this.DatabasesComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DatabasesComboBox.Enabled = false;
+            this.DatabasesComboBox.FormattingEnabled = true;
+            this.DatabasesComboBox.Location = new System.Drawing.Point(113, 54);
+            this.DatabasesComboBox.Name = "DatabasesComboBox";
+            this.DatabasesComboBox.Size = new System.Drawing.Size(495, 24);
+            this.DatabasesComboBox.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 592);
+            this.Controls.Add(this.DatabasesComboBox);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UserIdTextBox);
+            this.Controls.Add(this.DataSourceTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.UserIdCheckBox);
+            this.Name = "MainForm";
+            this.Text = "Connection Handler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox DataSourceTextBox;
+        private System.Windows.Forms.CheckBox UserIdCheckBox;
+        private System.Windows.Forms.TextBox UserIdTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox DatabasesComboBox;
+    }
+}
+
---
+++ ConnectionHandler/MainForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectionHandler
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UserIdCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            UserIdTextBox.Enabled = PasswordTextBox.Enabled = UserIdCheckBox.Checked;
+            if (UserIdCheckBox.Checked)
+                UserIdTextBox.Focus();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            DatabasesComboBox.Enabled = false;
+            DatabasesComboBox.Items.Clear();
+
+            var conStr = new SqlConnectionStringBuilder();
+            conStr.DataSource = DataSourceTextBox.Text;
+            conStr.InitialCatalog = "master";
+            if (UserIdCheckBox.Checked)
+            {
+                conStr.IntegratedSecurity = false;
+                conStr.UserID = UserIdTextBox.Text;
+                conStr.Password = PasswordTextBox.Text;
+            }
+            else
+                conStr.IntegratedSecurity=true;
+
+
+
+
+            using (var con = new SqlConnection(conStr.ConnectionString))
+            {
+                con.Open();
+                var com = new SqlCommand();
+                com.Connection = con;
+                com.CommandText = "select * from sys.databases";
+                var reader = com.ExecuteReader();
+
+
+                while (reader.Read())
+                {
+                    DatabasesComboBox.Enabled = true;
+                    DatabasesComboBox.Items.Add(reader["name"]);
+                }
+
+
+
+            }
+
+
+
+
+        }
+    }
+}
