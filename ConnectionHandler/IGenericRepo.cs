421388c70d527b770b2c6b0d28a9ae754b642976
Saeed MJ <treegp@gmail.com>
2022-07-21 13:44:08 +0430

    Rebuild Interface

---
+++ ConnectionHandler/IGenericRepo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ConnectionHandler.EntityAbstracts
+{
+    public interface IGenericRepo<TEntity>
+
+    {
+        int Insert(TEntity entity);
+        int Delete(TEntity entity);
+        int Update(TEntity entity);
+        List<TEntity> GetAll();
+        TEntity Top();
+        int Count();
+    }
+}
